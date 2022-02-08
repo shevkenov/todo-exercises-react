ae4314d80a486c91dcdf0bbc2559f62140e0d995
Andrey SHevkenov <shevkenov@gmail.com>
2022-02-08 20:09:39 +0200

    add: heaader component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import './Hease.css'
+import './Header.css'
 
-const Header = () => {
+const Header = (prop) => {
   return (
     <header>
-      
+      <button className={prop.type === 'all' ? 'active' : ''} onClick={() => prop.filter('all')}>view all</button>
+      <button className={prop.type === 'completed' ? 'active' : ''} onClick={() => prop.filter('completed')}>completed</button>
+      <button className={prop.type === 'pending' ? 'active' : ''} onClick={() => prop.filter('pending')}>pending</button>
     </header>
   );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,11 @@ import React, { useState } from 'react';
 import "./Home.css";
 import data from '../../store/data.json';
 import Item from '../Item/Item';
+import Header from '../Header/Header';
 
 const Home = () => {
   const [exercises, setExercises] = useState(data.exercises);
+  const [current, setCurrent] = useState('all');
 
   const removeItem = (id) => setExercises(prevValue => prevValue.filter(exercise => exercise.id !== id));
   const toggle = (id) => setExercises(prevValue => prevValue.map(exercise => {
@@ -14,12 +16,23 @@ const Home = () => {
     };
     
     return exercise
-  }))
+  }));
+
+  let jsx;
+
+  if(current === 'completed'){
+    jsx = exercises.filter(arr => arr.completed).map(item => <Item key={item.id} item={item} toggle={toggle} remove={removeItem}/>)
+  }else if(current === 'pending'){
+    jsx = exercises.filter(arr => !arr.completed).map(item => <Item key={item.id} item={item} toggle={toggle} remove={removeItem}/>)
+  }else{
+    jsx = exercises.map(item => <Item key={item.id} item={item} toggle={toggle} remove={removeItem}/>)
+  }
 
   return (
     <div className='home'>
+      <Header type={current} filter={setCurrent}/>
       {
-        exercises && exercises.map(item => <Item key={item.id} item={item} toggle={toggle} remove={removeItem}/>)
+        exercises && jsx
       }
     </div>
   );
